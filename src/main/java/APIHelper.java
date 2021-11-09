107b0ab896cddee75c0920806f45f9012e1d6b8a
Akash Das <akash.das@kyndryl.com>
2021-11-10 02:48:45 +0530

    added basic helper methods using rest assured lib for reusability

--- src/main/java/APIHelper.java
+++ src/main/java/APIHelper.java
@@ -1,12 +1,65 @@
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import io.restassured.http.ContentType;
 
 public class APIHelper {
 	
-	private String token = "";
+	private static String token = System.getProperty("apikey");
+	private static String baseURL = System.getProperty("baseurl");
 	
-	public APIHelper() {
-		// TODO Auto-generated constructor stub
-		this.token = System.getProperty("apikey");
-	}
-	
-	
+
+    public static Response post(String path, String requestPayload){
+    	String apiPath = baseURL + path;
+        return given().
+        		contentType(ContentType.JSON).
+        		accept(ContentType.JSON).
+        		header("Authorization", "Bearer "+token).
+                body(requestPayload).
+                when().
+                post(apiPath).
+                then().
+                extract().
+                response();
+    }
+
+    public static Response get(String path){
+    	String apiPath = baseURL + path;
+        return given().
+        		contentType(ContentType.JSON).
+        		accept(ContentType.JSON).
+        		header("Authorization", "Bearer "+token).
+        		when().
+        		get(apiPath).
+        		then().
+        		extract().
+        		response();
+    }
+
+    public static Response put(String path, String requestPayload){
+    	String apiPath = baseURL + path;
+        return given().
+        		contentType(ContentType.JSON).
+        		accept(ContentType.JSON).
+        		header("Authorization", "Bearer "+token).
+                body(requestPayload).
+                when().
+                put(apiPath).
+                then().
+                extract().
+                response();
+    }
+    
+    public static Response delete(String path, String requestPayload){
+    	String apiPath = baseURL + path;
+        return given().
+        		contentType(ContentType.JSON).
+        		accept(ContentType.JSON).
+        		header("Authorization", "Bearer "+token).
+                body(requestPayload).
+                when().
+                delete(apiPath).
+                then().
+                extract().
+                response();
+    }
 }
