e10568644a1af926a77bb46a2ca2b8030bbc2c04
Akash Das <akash.das@kyndryl.com>
2021-11-10 02:54:37 +0530

    abstracted api layer and test layer

--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,9 +1,12 @@
 import static io.restassured.RestAssured.delete;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
 
 import org.hamcrest.Matchers;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
@@ -12,56 +15,20 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 
-public class SampleTest extends APIHelper {
-
-	final static String ROOT_URI = "https://api.m3o.com/v1/user";
+public class SampleTest extends UserAPI {
 	
-//	@Test
-//	public void createUser() {
-//		String token = System.getProperty("apikey");
-//		Response response = given().
-//				contentType(ContentType.JSON)
-//				.accept(ContentType.JSON)
-//				.header("Authorization", "Bearer "+token)
-//				.body("{\r\n"
-//						+ "  \"email\": \"jod123@example.com\",\r\n"
-//						+ "  \"id\": \"usrid-15\",\r\n"
-//						+ "  \"password\": \"mySecretPass12345\",\r\n"
-//						+ "  \"username\": \"usrname-15\"\r\n"
-//						+ "}")
-//				.when()
-//				.post(ROOT_URI + "/Create");
-//		System.out.println("Post Response::\n"+ response.asString());
-//	}
+	@Test
+	public void createUserTest() {
+		Response res = UserAPI.createUser("abcdfe@example.com", "new31234");
+		Assert.assertEquals(res.statusCode(), 200);
+		System.out.println("Post Response::\n"+ res.asString());
+	}
 	
-//	@Test
-//	public void UpdateUser() {
-//		String token = System.getProperty("apikey");
-//		Response response = given().
-//				contentType(ContentType.JSON)
-//				.accept(ContentType.JSON)
-//				.header("Authorization", "Bearer "+token)
-//				.body("{\r\n"
-//						+ "  \"email\": \"jod13@example.com\",\r\n"
-//						+ "  \"id\": \"usrid-15\"\r\n"
-//						+ "}")
-//				.when()
-//				.post(ROOT_URI + "/Update");
-//		System.out.println("Post Response::\n"+ response.asString());
-//	}
-//	
+	@Test
+	public void UpdateUser() {
+	}
+	
 	@Test
 	public void deleteUser() {
-		String token = System.getProperty("apikey");
-		Response response = given().
-				contentType(ContentType.JSON)
-				.accept(ContentType.JSON)
-				.header("Authorization", "Bearer "+token)
-				.body("{\r\n"
-						+ "  \"id\": \"usrid-15\"\r\n"
-						+ "}")
-				.when()
-				.post(ROOT_URI + "/Delete");
-		System.out.println("Post Response::\n"+ response.asString());
 	}
 }
